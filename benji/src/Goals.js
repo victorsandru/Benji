943e84973bd8e19a6f5afd041e2bbe7db6cb12d4
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-06-21 11:24:32 -0400

    Add body styling using styled-components in Goals.js

--- benji/src/Goals.js
+++ benji/src/Goals.js
@@ -1,29 +1,38 @@
-import { Fragment } from "react";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import "./Goals.css";
 import TopNavigationBar from "./components/TopNavigationBar";
 import Section from "./components/Section";
+import { Fragment } from "react";
 
+// Use styled-components to specify the sizes of two sections
 const SmallerSection = styled(Section)`
-  width: 75em;
+  width: 70vw;
   height: 15rem;
 `;
 
 const LargerSection = styled(Section)`
-  width: 75em;
+  width: 70vw;
   height: 42rem;
   margin-top: 3rem;
+`;
+
+// Create global style to apply gray background
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #F5F5F5;
+  }
 `;
 
 function Goals() {
   return (
     <Fragment>
+      <GlobalStyle />
       <TopNavigationBar />
       <SmallerSection>
-        <h2>Goals</h2>
+        <h1>Goals</h1>
       </SmallerSection>
       <LargerSection>
-        <h2>Summary</h2>
+        <h1>Summary</h1>
       </LargerSection>
     </Fragment>
   );
