f7b7c4b851eebadeb44a373038214e44b0c4d7c2
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-06-20 20:41:30 -0400

    Add TopNavigationBar component

---
+++ benji/src/components/TopNavigationBar.js
@@ -0,0 +1,16 @@
+/*
+ * This element shows up as a colored bar at the top of the page.
+ */
+
+import "./TopNavigationBar.css";
+import "../Goals.css";
+
+const TopNavigationBar = () => {
+  return (
+    <header className={"gradient-1-background"}>
+      <h1>Benji</h1>
+    </header>
+  );
+};
+
+export default TopNavigationBar;
