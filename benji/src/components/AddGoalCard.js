52ec0283f4e5b9b6a2f6097b02eaafa5b530dce3
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-06-22 14:29:41 -0400

    Adjust gradient in the AddGoal button

--- benji/src/components/AddGoalCard.js
+++ benji/src/components/AddGoalCard.js
@@ -19,7 +19,7 @@ const AddGoalCard = () => {
             <linearGradient id="gradient2">
               <stop offset="5.87%" stopColor="rgba(134, 174, 203, 0.74)" />
               <stop offset="5.88%" stopColor="rgba(134, 174, 203, 0.6956)" />
-              <stop offset="93.54%" stopColor="rgba(179, 133, 201, 0.2442)" />
+              <stop offset="93.54%" stopColor="rgba(179, 133, 201, 0.4)" />
             </linearGradient>
           </defs>
           <path
