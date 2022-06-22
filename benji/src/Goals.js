7aa0b4049e7301a7513cff1637d1438ce2435f9b
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-06-22 13:39:41 -0400

    Populate the Goals page with summary cards and add vertical bars in-between

--- benji/src/Goals.js
+++ benji/src/Goals.js
@@ -3,6 +3,7 @@ import "./Goals.css";
 import TopNavigationBar from "./components/TopNavigationBar";
 import Section from "./components/Section";
 import SummaryCard from "./components/SummaryCard";
+import AddGoalCard from "./components/AddGoalCard";
 import { Fragment } from "react";
 
 // Use styled-components to specify the sizes of two sections
@@ -24,6 +25,14 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+// Create vertical bar to separate summary cards
+const VerticalBar = styled.div`
+  width: 0;
+  height: 7rem;
+  margin: 1.25rem 2.5rem 0;
+  border: 1px solid rgba(201, 201, 201, 0.9);
+`;
+
 function Goals() {
   return (
     <Fragment>
@@ -31,8 +40,15 @@ function Goals() {
       <TopNavigationBar />
       <SmallerSection>
         <h1 className={"section-header"}>Your Goals</h1>
-        <br />
         <SummaryCard data={5} category={"active"} />
+        <VerticalBar />
+        <SummaryCard data={10} category={"completed"} />
+        <VerticalBar />
+        <SummaryCard data={1} category={"due today"} />
+        <VerticalBar />
+        <SummaryCard data={"37%"} category={"progress"} />
+        <VerticalBar />
+        <AddGoalCard />
       </SmallerSection>
 
       <LargerSection></LargerSection>
