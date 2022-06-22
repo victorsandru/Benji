5c44d703b5ee78666b4ff6e8883ff45851ea9c00
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-06-21 21:47:45 -0400

    Center app name in the TopNavigationBar component

--- benji/src/components/TopNavigationBar.js
+++ benji/src/components/TopNavigationBar.js
@@ -2,13 +2,13 @@
  * This element shows up as a colored bar at the top of the page.
  */
 
-import classes from "./TopNavigationBar.module.css";
+import "./TopNavigationBar.css";
 import "../Goals.css";
 
 const TopNavigationBar = () => {
   return (
     <header className={"gradient-1-background"}>
-      <h1 className={classes.appName}>Benji</h1>
+      <h1 className={"app-name"}>Benji</h1>
     </header>
   );
 };
