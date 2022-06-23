7a4c7660b91d661f8b697cbf8eacfb87da3175fa
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-06-23 10:42:35 -0400

    Add GoalCard component for displaying information about each goal

---
+++ benji/src/components/GoalCard.js
@@ -0,0 +1,11 @@
+/*
+ * This component will show information about the goal.
+ */
+
+import "./GoalCard.css";
+
+const GoalCard = () => {
+  return <div className={"goal-card"}></div>;
+};
+
+export default GoalCard;
