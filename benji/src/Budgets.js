05476dfb42959f18c872ce5de3a8208a1753c1b2
Haydenchung03 <Haydenchung21@hotmail.com>
2022-06-25 15:56:45 -0400

    Added hoem button

--- benji/src/Budgets.js
+++ benji/src/Budgets.js
@@ -4,7 +4,9 @@ function Budgets() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Budgets</h1>
+        <h1>Benji</h1>
+        <h2 div = "home"><a href = "http://192.168.56.1:3000/">Home</a></h2>
+        <h2>Welcome to the budget section of this finance app!</h2>
       </header>
     </div>
   );
