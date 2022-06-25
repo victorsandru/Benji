cd725f06d88efaa2903dc00ab3afad371e70f2e6
Haydenchung03 <Haydenchung21@hotmail.com>
2022-06-25 15:57:58 -0400

    Edit

--- benji/src/Budgets.js
+++ benji/src/Budgets.js
@@ -6,7 +6,7 @@ function Budgets() {
       <header className="App-header">
         <h1>Benji</h1>
         <h2 div = "home"><a href = "http://192.168.56.1:3000/">Home</a></h2>
-        <h2>Welcome to the budget section of this finance app!</h2>
+        <h2>Welcome to the budget section of this finance app</h2>
       </header>
     </div>
   );
