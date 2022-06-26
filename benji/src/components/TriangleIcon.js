52c5b4de7676ee697c479f20d83cc65a001530ed
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-06-26 17:01:10 -0400

    Adjust filling rule in TriangleIcon.js to fix error message

--- benji/src/components/TriangleIcon.js
+++ benji/src/components/TriangleIcon.js
@@ -13,7 +13,6 @@ const TriangleIcon = () => {
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          fillRule="full"
           clipRule="evenodd"
           d="M11.6603 5L3 20H20.3205L11.6603 5ZM11.6603 11L8.19615 17H15.1244L11.6603 11Z"
           fill="currentColor"
