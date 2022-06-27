62e6edaa9438e9fd2439a08e49d1041262d51d68
Haydenchung03 <Haydenchung21@hotmail.com>
2022-06-27 13:33:10 -0400

    Added two buttons for home

--- benji/src/Budgets.js
+++ benji/src/Budgets.js
@@ -5,7 +5,8 @@ function Budgets() {
     <div className="App">
       <header className="App-header">
         <h1>Benji</h1>
-        <h2 div = "home"><a href = "http://192.168.56.1:3000/">Home</a></h2>
+        <h2 div = "home1"><a href = "http://192.168.56.1:3000/">Local IP Home</a></h2>
+        <h2 div = "home2"><a href = "http://localhost:3000/">Local Host 3000</a></h2>
         <h2>Welcome to the budget section of this finance app</h2>
       </header>
     </div>
