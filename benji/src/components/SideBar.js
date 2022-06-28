a76d75120ff82e01a041d881b9af18f1ad72ebe6
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-06-28 15:12:37 -0400

    Add SideBar component

---
+++ benji/src/components/SideBar.js
@@ -0,0 +1,11 @@
+/*
+ * This component displays a sidebar at the left.
+ */
+
+import "./SideBar.css";
+
+const SideBar = () => {
+  return <div className={"sidebar"}></div>;
+};
+
+export default SideBar;
