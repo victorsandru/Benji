f2771565c67c3997e686f9aca42e824a8d44d1dd
Victor <victor@Victors-PC.local>
2022-07-20 13:49:29 -0400

    fixed sidebar bugs

--- benji/src/pages/Expenses.js
+++ benji/src/pages/Expenses.js
@@ -1,12 +1,11 @@
 import './Expenses.css';
+import BasePageLayout from '../components/UI/BasePageLayout';
 
 function Expenses() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Expenses</h1>
-      </header>
-    </div>
+    <>
+      <BasePageLayout />
+    </>
   );
 }
 
