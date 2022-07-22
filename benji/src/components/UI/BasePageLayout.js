bf24e69eccb7eb20585921246895ec045bb1b92f
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-07-22 08:36:02 -0400

    Adjust styling for the BasePageLayout

--- benji/src/components/UI/BasePageLayout.js
+++ benji/src/components/UI/BasePageLayout.js
@@ -1,5 +1,8 @@
 /*
  * This component render the base layout for the page.
+ *
+ * This component will accept an array of CSS classes for styling
+ * the container that wraps the main content of the home page.
  */
 
 import { Fragment } from "react";
@@ -7,13 +10,20 @@ import "./BasePageLayout.css";
 import TopNavigationBar from "./TopNavigationBar";
 import SideBar from "./SideBar";
 
+const SectionsStyling = {
+  margin: "0 auto",
+  height: "100%",
+};
+
 const BasePageLayout = (props) => {
   return (
     <Fragment>
       <TopNavigationBar />
       <main>
         <SideBar />
-        <div className={"sections"}>{props.children}</div>
+        <div style={SectionsStyling} className={props.classes?.join(" ")}>
+          {props.children}
+        </div>
       </main>
     </Fragment>
   );
