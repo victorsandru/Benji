75d70f49d28d98e738c8bfd0ca405a4840b287d9
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-07-23 14:44:56 -0400

    Use props for styling ProgressBar

--- benji/src/components/ProgressBar/ProgressBar.js
+++ benji/src/components/ProgressBar/ProgressBar.js
@@ -7,21 +7,22 @@ import "./ProgressBar.css";
  */
 const ProgressBar = (props) => {
   return (
-    <div className={`bar-container bar-container-${props.barType}`}>
-      <div className={`bar-bg bar-bg-${props.barType}`}>
+    <div className={`bar-container bar-container--${props.barType}`}>
+      <div className={`bar-bg bar-bg--${props.barType}`}>
         <div
-          className={`bar bar-fill-${props.barFill}`}
+          className={"bar"}
           style={{
             width: `${props.progress}%`,
+            background: props.barFill,
           }}
         >
           <p
-            className={`saved-amount saved-amount-${props.barType}`}
+            className={`saved-amount saved-amount--${props.barType}`}
           >{`$${props.savedAmount.toLocaleString("en-US")}`}</p>
         </div>
       </div>
       <p
-        className={`total-amount total-amount-${props.barType}`}
+        className={`total-amount total-amount--${props.barType}`}
       >{`$${props.goalAmount.toLocaleString("en-US")}`}</p>
     </div>
   );
