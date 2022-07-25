1e230916d438c68700788cdc8cd24bbe1cdd5697
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-07-25 16:45:18 -0400

    Add file with dummy budget data

---
+++ benji/src/DummyBudgetData.js
@@ -0,0 +1,24 @@
+const budget = {
+  total: 2300,
+  spent: 1450,
+  categories: [
+    {
+      name: "rent",
+      amount: 1000,
+    },
+    {
+      name: "groceries",
+      amount: 800,
+    },
+    {
+      name: "dining",
+      amount: 300,
+    },
+    {
+      name: "internet",
+      amount: 200,
+    },
+  ],
+};
+
+export default budget;
