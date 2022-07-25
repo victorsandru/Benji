d1f0c1291820d80bca51739f31edb2fab07f19c2
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-07-25 16:52:44 -0400

    Adjust styling and props in the BudgetPieChart.js

--- benji/src/components/BudgetPieChart/BudgetPieChart.js
+++ benji/src/components/BudgetPieChart/BudgetPieChart.js
@@ -1,39 +1,34 @@
-import drawPieChart from "./DrawPieChart";
-import { useEffect, useRef } from "react";
+import drawPieChart from "./drawPieChart";
+import { useEffect, useRef, useState } from "react";
 
 /**
  * This component renders a pie chart using D3.
  *
- * Accepts data and size as props.
+ * Accepts data, size and segment colors as props.
  * */
 const BudgetPieChart = (props) => {
+  // Trigger opacity transition for the label inside the pie chart
+  const [opacity, setOpacity] = useState(0);
+  useEffect(() => setOpacity(1), []);
+
   // Define container style
   const ChartContainerStyle = {
     width: props.width,
     height: props.height,
-    margin: "2.5% 0 2.5% 2.5%",
+    margin: "3.5% 0 1.5% 2.5%",
     position: "relative",
   };
 
-  const data = [
-    { value: 40 },
-    { value: 25 },
-    { value: 15 },
-    { value: 8 },
-    { value: 2 },
-  ];
-
-  // Array of colors that will be used for styling segments
-  const colors = ["#05BBD2", "#2070C4", "#EB80F1", "#F5C842", "#37D400"];
-
   const containerRef = useRef();
   useEffect(() => {
-    drawPieChart(containerRef.current, data, colors);
+    drawPieChart(containerRef.current, props.data, props.colors);
   }, [containerRef]);
 
   return (
     <div style={ChartContainerStyle} ref={containerRef}>
-      <div>{props.children}</div>
+      <div style={{ opacity, transition: "opacity 0.7s ease-in" }}>
+        {props.children}
+      </div>
     </div>
   );
 };
