90f51bed9385d4eb08a73633d4af2cfdbffa3805
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-08-22 16:23:05 -0400

    Adjust width of the TopNavigationBar component

--- benji/src/components/UI/BasePageLayout/BasePageLayout.js
+++ benji/src/components/UI/BasePageLayout/BasePageLayout.js
@@ -7,15 +7,28 @@ import SideBar from "../SideBar";
  * This component render the base layout for the page.
  *
  * This component will accept an array of CSS classes for styling
- * the container that wraps the main content of the home page.
+ * the container that wraps the main content of the home page. It
+ * also accepts the CSS styles for the main element and TopNavigationBar.
  */
-const BasePageLayout = (props) => {
+const BasePageLayout = ({
+  classes,
+  pageMinWidth = "100%",
+  children = null,
+}) => {
   return (
     <Fragment>
-      <TopNavigationBar />
-      <main>
+      <TopNavigationBar
+        styles={{
+          minWidth: pageMinWidth,
+        }}
+      />
+      <main
+        style={{
+          minWidth: pageMinWidth,
+        }}
+      >
         <SideBar />
-        <div className={props.classes?.join(" ")}>{props.children}</div>
+        <div className={classes?.join(" ")}>{children}</div>
       </main>
     </Fragment>
   );
--- benji/src/components/UI/TopNavigationBar/TopNavigationBar.js
+++ benji/src/components/UI/TopNavigationBar/TopNavigationBar.js
@@ -1,12 +1,12 @@
-/*
- * This element shows up as a colored bar at the top of the page.
- */
-
 import "./TopNavigationBar.css";
 
-const TopNavigationBar = () => {
+/**
+ * This element shows up as a colored bar at the top of the page.
+ * Takes styles object as props which defines the header style.
+ */
+const TopNavigationBar = (props) => {
   return (
-    <header className={"navigation-header"}>
+    <header className={"navigation-header"} style={props.styles}>
       <h1 className={"app-name"}>Benji</h1>
     </header>
   );
