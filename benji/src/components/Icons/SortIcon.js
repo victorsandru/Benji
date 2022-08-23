7d5aa9d5f5e8453abe168d26bc7ef6395c0622a3
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-08-23 13:56:29 -0400

    Add sort and filter icons as separate components

---
+++ benji/src/components/Icons/SortIcon.js
@@ -0,0 +1,18 @@
+export default function SortIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="-1 -4 18 18"
+      stroke="currentColor"
+      strokeWidth={1}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3 4h12M3 6h9m-9 2h6m-6 2h5"
+      />
+    </svg>
+  );
+}
