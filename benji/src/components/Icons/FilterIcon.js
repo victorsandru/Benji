4882e892375e3c1c2ed978f12ef845a05bbe31cf
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-08-24 20:19:00 -0400

    Update several components containing icons

--- benji/src/components/Icons/FilterIcon.js
+++ benji/src/components/Icons/FilterIcon.js
@@ -2,16 +2,18 @@ export default function FilterIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className={"svg-icon-filter"}
       fill="none"
-      viewBox="2 -1 22 22"
+      height={"20"}
+      width={"20"}
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
       stroke="currentColor"
-      strokeWidth={2}
+      className="w-6 h-6 svg-filter"
     >
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
-        d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"
+        d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
       />
     </svg>
   );
--- benji/src/components/Icons/TriangleIcon.js
+++ benji/src/components/Icons/TriangleIcon.js
@@ -2,24 +2,23 @@
  * This component renders the triangle icon used in the filter and sort buttons.
  */
 
-const TriangleIcon = () => {
+const TriangleIcon = ({ clicked = false }) => {
   return (
-    <div className={"svg-container-triangle"}>
-      <svg
-        width="24"
-        height="20"
-        viewBox="0 0 34 34"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className={"svg-triangle"}
-      >
-        <path
-          clipRule="evenodd"
-          d="M11.6603 5L3 20H20.3205L11.6603 5ZM11.6603 11L8.19615 17H15.1244L11.6603 11Z"
-          fill="currentColor"
-        />
-      </svg>
-    </div>
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={`svg-triangle${clicked ? "-clicked" : ""}`}
+    >
+      <path
+        fillRule="nonzero"
+        clipRule="evenodd"
+        d="M11.6603 5L3 20H20.3205L11.6603 5ZM11.6603 11L8.19615 17H15.1244L11.6603 11Z"
+        fill="currentColor"
+      />
+    </svg>
   );
 };
 
