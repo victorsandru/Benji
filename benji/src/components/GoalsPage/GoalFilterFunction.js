977a4cd394e3fb4fe3d592ef95b3b7081e91b2e8
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-08-29 14:58:31 -0400

    Adjust filter logic in GoalFilterFunction.js

--- benji/src/components/GoalsPage/GoalFilterFunction.js
+++ benji/src/components/GoalsPage/GoalFilterFunction.js
@@ -7,8 +7,6 @@
  * @return filteredGoals - filtered list of goals.
  */
 export default function filterGoals(goals, filterOptions, appliedFilters) {
-  console.log("Filtering goals...");
-
   Object.keys(filterOptions).forEach((filterType) => {
     // Select applied filters for each type of filter
     const selectedFiltersByType = filterOptions[filterType].filter((filter) =>
@@ -18,19 +16,22 @@ export default function filterGoals(goals, filterOptions, appliedFilters) {
     if (selectedFiltersByType.length > 0) {
       goals = goals.filter((goal) => {
         return selectedFiltersByType.reduce((prevFilterResult, curFilter) => {
+          let includeGoal;
+
           if (filterType === "date") {
-            return applyDateFilter(
-              new Date(goal.dueDate),
-              Object.keys(curFilter)[0],
-              prevFilterResult
-            );
+            includeGoal =
+              prevFilterResult ||
+              applyDateFilter(
+                new Date(goal.dueDate),
+                Object.keys(curFilter)[0]
+              );
           } else if (filterType === "status") {
-            return applyStatusFilter(
-              goal,
-              Object.keys(curFilter)[0],
-              prevFilterResult
-            );
+            includeGoal =
+              prevFilterResult ||
+              applyStatusFilter(goal, Object.keys(curFilter)[0]);
           }
+
+          return includeGoal;
         }, false);
       });
     }
@@ -40,45 +41,45 @@ export default function filterGoals(goals, filterOptions, appliedFilters) {
 }
 
 // Helper functions
-function applyDateFilter(goalDueDate, curFiler, prevFilterResult) {
+function applyDateFilter(goalDueDate, curFiler) {
   const today = new Date();
 
   switch (curFiler) {
     case "today":
-      return prevFilterResult || goalDueDate <= today;
+      return goalDueDate <= today;
     case "tomorrow":
       const tomorrow = new Date().setDate(today.getDate() + 1);
-      return prevFilterResult || goalDueDate <= tomorrow;
+      return goalDueDate <= tomorrow;
     case "thisWeek":
       const lastDayThisWeek = new Date().setDate(
         today.getDate() + (7 - (today.getDay() % 7))
       );
-      return prevFilterResult || goalDueDate <= lastDayThisWeek;
+      return goalDueDate <= lastDayThisWeek;
     case "thisMonth":
       const lastDayThisMonth = new Date(
         today.getFullYear(),
         today.getMonth() + 1,
         0
       );
-      return prevFilterResult || goalDueDate <= lastDayThisMonth;
+      return goalDueDate <= lastDayThisMonth;
     case "thisYear":
       const lastDayThisYear = new Date(today.getFullYear() + 1, 0, 0);
-      return prevFilterResult || goalDueDate <= lastDayThisYear;
+      return goalDueDate <= lastDayThisYear;
     default:
-      return prevFilterResult || true;
+      return true;
   }
 }
 
-function applyStatusFilter(goal, curFilter, prevFilterResult) {
+function applyStatusFilter(goal, curFilter) {
   switch (curFilter) {
     case "complete":
-      return prevFilterResult || goal.savedAmount >= goal.goalAmount;
+      return goal.savedAmount >= goal.goalAmount;
     case "incomplete":
-      return prevFilterResult || goal.savedAmount < goal.goalAmount;
+      return goal.savedAmount < goal.goalAmount;
     case "active":
-      return prevFilterResult || goal.active;
+      return goal.active;
     case "inactive":
-      return prevFilterResult || !goal.active;
+      return !goal.active;
     default:
       return true;
   }
