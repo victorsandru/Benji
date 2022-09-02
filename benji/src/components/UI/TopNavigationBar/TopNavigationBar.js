4f7f10bc4699e2fbb505e9580aa6670f8d541507
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2022-09-02 14:44:58 -0400

    Add helper message to the top navigation bar

--- benji/src/components/UI/TopNavigationBar/TopNavigationBar.js
+++ benji/src/components/UI/TopNavigationBar/TopNavigationBar.js
@@ -1,13 +1,31 @@
 import "./TopNavigationBar.css";
+import { useLocation } from "react-router-dom";
 
 /**
  * This element shows up as a colored bar at the top of the page.
  * Takes styles object as props which defines the header style.
  */
 const TopNavigationBar = (props) => {
+  const { pathname } = useLocation();
+  const message = (() => {
+    switch (pathname.slice(1)) {
+      case "expenses":
+        return "Your Expenses";
+      case "budget":
+        return "Your Budget";
+      case "goals":
+        return "Your Goals";
+      case "calendar":
+        return "Your Calendar";
+      default:
+        return "Welcome!";
+    }
+  })();
+
   return (
     <header className={"navigation-header"} style={props.styles}>
       <h1 className={"app-name"}>Benji</h1>
+      <h1 className={"navigation-message"}>{message}</h1>
     </header>
   );
 };
