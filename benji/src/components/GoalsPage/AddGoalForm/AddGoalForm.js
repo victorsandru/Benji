e53f671e80cf0a72072a07ff439a89cac028395c
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2023-01-05 08:47:38 +0300

    Add template for AddGoalForm component

---
+++ benji/src/components/GoalsPage/AddGoalForm/AddGoalForm.js
@@ -0,0 +1,18 @@
+import "./AddGoalForm.css";
+
+/**
+ * Renders form for adding goals.
+ * @param props
+ */
+export default function AddGoalForm(props) {
+  return (
+    <form>
+      <div>
+        <h2>Goal Information</h2>
+      </div>
+      <div>
+        <button>Cancel</button>
+      </div>
+    </form>
+  );
+}
